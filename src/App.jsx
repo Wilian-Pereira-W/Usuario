fa80bec4b767dade4735426291dfe0aad63516cb
Wilian Pereira <wilian_pereira92@outlook.com>
2022-05-10 10:04:16 -0300

    feat: implementado a rota Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './styles/global.scss';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
