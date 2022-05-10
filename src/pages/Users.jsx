6d725fecfe40f1261bb8dd5662eea678e80e8dae
Wilian Pereira <wilian_pereira92@outlook.com>
2022-05-10 20:19:42 -0300

    feat: importando o componente User

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,27 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import api from '../services/api';
+import React from 'react';
+import User from '../components/User';
 
 function Users() {
-  const [users, setUser] = useState();
-
-  useEffect(() => {
-    const key = JSON.parse(localStorage.getItem('key'));
-    api
-      .get('users', {
-        headers: {
-          Authorization: `bearer ${key}`,
-        },
-      })
-      .then((response) => setUser(response.data))
-      .catch((err) => {
-        console.error(`ops! ocorreu um erro${err}`);
-      });
-  }, []);
-
   return (
-    <div>
-      <h1>User</h1>
-    </div>
+    <User />
   );
 }
 
