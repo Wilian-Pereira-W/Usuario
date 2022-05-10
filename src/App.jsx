e14018e78d6029edb757c818ee8295aa3054bba1
Wilian Pereira <wilian_pereira92@outlook.com>
2022-05-10 11:38:38 -0300

    feat: implementado a rota register

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import {
   Route,
 } from 'react-router-dom';
 import Home from './pages/Home';
+import Register from './pages/Register';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
